93ba7c0f01ea687072e6d8e0ab53726f8bb84456
Andii Shapoval <andrii.shapoval@smile-ukraine.com>
2022-04-19 18:58:34 +0300

    added cart items

--- crwn-clothing/src/components/card-dropdown/card-dropdown.component.jsx
+++ crwn-clothing/src/components/card-dropdown/card-dropdown.component.jsx
@@ -1,12 +1,19 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.component';
 import './card-dropdown.styles.scss';
 import CartItem from '../cart-icon/cart-item/cart-item.component';
 
+
 const CardDropdown = () => {
+
+    const {cartItems} = useContext(CartContext);
+
     return (
         <div className='cart-dropdown-container'>
             <div className="cart-items">
-                {[].map(item => <CartItem cartItem={item} />)}
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
             </div>
             <Button>Go to checkout</Button>
         </div>
