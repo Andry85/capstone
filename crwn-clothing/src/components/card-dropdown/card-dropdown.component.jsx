16561cbef9fcf18b9aa355e972b2069333c59fb4
Andii Shapoval <andrii.shapoval@smile-ukraine.com>
2022-04-20 12:37:21 +0300

    added checkout page

--- crwn-clothing/src/components/card-dropdown/card-dropdown.component.jsx
+++ crwn-clothing/src/components/card-dropdown/card-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
+import { useNavigate } from "react-router-dom";
 
 import Button from '../button/button.component';
 import './card-dropdown.styles.scss';
@@ -9,13 +10,18 @@ import CartItem from '../cart-icon/cart-item/cart-item.component';
 const CardDropdown = () => {
 
     const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const gotToCheckoutHandler = () => {
+        navigate('/checkout');
+    }
 
     return (
         <div className='cart-dropdown-container'>
             <div className="cart-items">
                 {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
             </div>
-            <Button>Go to checkout</Button>
+            <Button onClick={gotToCheckoutHandler}>Go to checkout</Button>
         </div>
     );
 }
