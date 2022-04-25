6ce2740f0c37e327c55ff1e3c9506a6e2eee9106
Andii Shapoval <andrii.shapoval@smile-ukraine.com>
2022-04-25 11:37:17 +0300

    added data to firestore

--- crwn-clothing/src/contexts/product.context.jsx
+++ crwn-clothing/src/contexts/product.context.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from '../shop-data.json';
+import {addCollectionAndDocuments} from '../utils/firebase/firebase.utils';
+
 
 
 export const ProductsContext = createContext({
@@ -9,7 +10,9 @@ export const ProductsContext = createContext({
 
 export const ProductProvider = ({children}) => {
 
-    const [products, setProducts] = useState(PRODUCTS)
+    const [products, setProducts] = useState([]);
+
+
     const value = {products};
 
     return (
--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -9,7 +9,14 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import {
+    getFirestore, 
+    doc, 
+    getDoc, 
+    setDoc, 
+    collection,
+    writeBatch
+  } from 'firebase/firestore';
 
 
 // Your web app's Firebase configuration
@@ -37,6 +44,25 @@ const firebaseConfig = {
 
 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (
+    collectionKey, 
+    objectsToAdd,
+    field = 'title'
+    ) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    });
+
+    await batch.commit();
+
+    console.log('done');
+
+  }
 
   export const createUserDocumentfromAuth = async(
     userAuth, 
